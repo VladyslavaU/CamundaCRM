03f3c69006625c75a43e7befce352f1aa073caeb
VladyslavaU <vuvarova@outlook.com>
2023-06-13 15:05:00 +0300

    Initial commit

---
+++ src/main/java/com/camundacrm/CamundaCrmApplication.java
@@ -0,0 +1,14 @@
+package com.camundacrm;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaCrmApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/camundacrm/SetStatus.java
@@ -0,0 +1,11 @@
+package com.camundacrm;
+
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SetStatus {
+
+
+}
